eacbf1a37d9fddb7bf28fadd1db6b2c8edb16cf9
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2025-07-24 13:24:08 -0400

    Update 3_chunk_data.py
    Added more comments

--- 3_chunk_data.py
+++ 3_chunk_data.py
@@ -1,9 +1,20 @@
 from pathlib import Path
 from utils import chunk_image
 
-# Define the file path using pathlib
+
+# USER PARAMETERS
+
+# Give the path to the files you would like to chunk
+# The path can be to raw images or atlas slices
 file_path = Path(r"Z:\Labmembers\Ingvild\Cellpose\NeuN_model\training_sections\\")
+
+# Define the chunk size. 
+# Smaller chunks (up to 128) are preferred for more dense signals (NeuN, Sytox)
+# Larger chunks (e.g. 256, 512) could be useful for medium to very sparse signals
 chunk_size = 128
+
+
+# MAIN CODE, do not edit
 
 # Glob for TIFF files using pathlib
 files = file_path.glob("*.tif")
