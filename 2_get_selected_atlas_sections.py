1d51e8649658559d08019f1b55be69e939564a46
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2025-07-24 10:44:54 -0400

    Update 2_get_selected_atlas_sections.py
    Restructuring code

--- 2_get_selected_atlas_sections.py
+++ 2_get_selected_atlas_sections.py
@@ -4,35 +4,44 @@ from glob import glob
 from PIL import Image
 import numpy as np
 import cv2
+from pathlib import Path
 
-# Enter paths
-base_path = r"Z:\Labmembers\Ingvild\Testing_CellPose\test_data\\"
-out_path = r"Z:\Labmembers\Ingvild\Testing_CellPose\test_data\Ex_488_Ch0_stitched_selected_data\\"
+# USER PARAMETERS
+# Give the path of your sample folder
+sample_path = Path(r"Z:\LSFM\2025\2025_03\2025_03_28\20250328_10_45_32_NB_CS0303_F_P428_C57_LAS_488Lectin_561NeuN_640Iba1_4x_4umstep_Destripe_DONE\\")
 
-all_images_path = glob(f"{base_path}Ex_488_Ch0_stitched\\*.tif")
-no_images = len(all_images_path)
+# Give the path to the images for which you want matching atlas plates. 
+# NB: Image names must not have been changed from their original naming format.
+selected_images_path = Path(r"Z:\Labmembers\Ingvild\Testing_CellPose\selected_images\\")
 
-image_path = rf"{base_path}Ex_488_Ch0_stitched_selected_data\\"
-subset_images = glob(f"{image_path}*.tif")
 
-# Access image data and find the relationship between the size of the z axis in atlas vol versus image data
-file_path = rf'{base_path}ANTs_TransformedImage.nii.gz'
-nifti_img = nib.load(file_path)
-data = np.asanyarray(nifti_img.dataobj)
-vol_axis_len = data.shape[1]
-axis_ratio = no_images / vol_axis_len 
+# MAIN CODE, do not edit
 
-for image in subset_images:
-    name = os.path.basename(image)
+all_images_path = sample_path / "Ex_488_Ch0_stitched"
+subset_images = selected_images_path.glob("*.tif")
+reg_vol_path = sample_path / "_01_registration" / "ANTs_TransformedImage.nii.gz"
 
+def extract_atlas_plate(reg_volume, image, all_images_path):
+
+    # Calculate total number of images
+    all_images = list(all_images_path.glob("*.tif"))
+    no_images = len(all_images)
+
+    # Access registered volume and find the relationship between the size of the z axis in atlas vol versus image data
+    nifti_img = nib.load(reg_volume)
+    data = np.asanyarray(nifti_img.dataobj)
+    vol_axis_len = data.shape[1]
+    axis_ratio = no_images / vol_axis_len 
+  
     # calculate the absolute index of the image slice
     # # the last number in the file name uses a 0-based indexing with an increment of 20
+    name = image.name
     number = int(name.split("_")[2])
     image_index = (number / 20)
 
     # find the corresponding z axis slice in the atlas volume, scaling by the axis ratio to get the right one
     atlas_index = int(np.round(image_index / axis_ratio))
-    horizontal_slice = data[:, atlas_index, :]
+    atlas_slice = data[:, atlas_index, :]
 
     # make the image into an array and get the width and height for scaling purposes
     image_slice = np.array(Image.open(image))
@@ -40,20 +49,29 @@ for image in subset_images:
 
     # rotate and scale the horizontal slice to the shape of the image slice
     # use no interpolation to ensure no changes in label values
-    rotated_horizontal_slice = np.rot90(horizontal_slice)
-    resized_horizontal_slice = cv2.resize(rotated_horizontal_slice, 
+    rotated_atlas_slice = np.rot90(atlas_slice)
+    resized_atlas_slice = cv2.resize(rotated_atlas_slice, 
                                           (target_shape[1], target_shape[0]), 
                                           interpolation=cv2.INTER_NEAREST)
+    
+    resized_atlas_slice_16bit = resized_atlas_slice.astype(np.uint16)
 
+    return name, resized_atlas_slice_16bit
+
+
+for image in subset_images:
+    name, atlas_slice = extract_atlas_plate(reg_vol_path, image, all_images_path)
+    image_data = np.array(Image.open(image))
+    print(name)
     # plot the slice using matplotlib for visual validation
-    plt.imshow(resized_horizontal_slice.T, cmap="gray", origin="lower", vmin=np.min(image_slice), vmax=np.max(image_slice) * 0.1)
-    plt.imshow(image_slice.T, cmap='gray', origin='lower', vmin=np.min(image_slice), vmax=np.max(image_slice) * 0.1, alpha=0.5)
-    plt.title(f"Horizontal Slice at Index {atlas_index}")
+    plt.imshow(atlas_slice.T, cmap="gray", origin="lower", vmin=np.min(image_data), vmax=np.max(image_data) * 0.1)
+    plt.imshow(image_data.T, cmap='gray', origin='lower', vmin=np.min(image_data), vmax=np.max(image_data) * 0.1, alpha=0.5)
+    plt.title(f"Horizontal Slice for {image.name}")
     plt.axis('off')
     plt.show()
 
     # save the atlas slice as a 16 bit tiff image
-    resized_horizontal_slice_16bit = resized_horizontal_slice.astype(np.uint16)
-    cv2.imwrite(rf'{out_path}{name.split(".")[0]}_atlas_slice.tif', resized_horizontal_slice_16bit)
+
+    cv2.imwrite(rf'{selected_images_path}\{name.split(".")[0]}_atlas_slice.tif', atlas_slice)
 
     print("Image has been saved successfully.")
