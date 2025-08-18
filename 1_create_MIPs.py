80266e4ea2db84493d8a3a499a6bc433e8e8d45d
Ingvild Bjerke <ingvieb@gmail.com>
2025-08-18 17:33:51 -0400

    Update 1_create_MIPs.py
    Support for working with single planes (facilitating normalization whether or not MIPs are made)

--- 1_create_MIPs.py
+++ 1_create_MIPs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import cv2
 import numpy as np
+import shutil
 from utils import create_mips_from_folder, normalize_image
 
 # USER PARAMETERS
@@ -8,15 +9,19 @@ from utils import create_mips_from_folder, normalize_image
 # Give your input folders 
 # (the path should be to the stitched folder of the channel to create MIPs for)
 # You can put as many as you'd like within the brackets, separated by comma
-input_folders = [Path(r"Z:\LSFM\2023\2023_12\20231211_JL_JL0810_M_P07_Fmr1_Npas4Ai75_LAS_488Bg_561TdTomato_4x_5umstep_LC_SUNY_A1\\"),
+input_folders = [Path(r"Z:\LSFM\2025\2025_06\2025_06_20\20250620_12_59_29_LJS_IEB0037_M_P56_Aldh1_LAS_488Bg_561NeuN_640Iba1_4x_4umstep_Destripe_DONE")
                  ]
 
 # MIP parameters
+
+# Set to False if you do not want to make MIPs
+create_MIPs = False
 z_step_size = 5  # Give the step size of your images
 mip_thickness = 20  # The desired thickness for MIP in micrometers
 channel = 1
  
 # Normalization parameters
+
 # Set to False if you do not want normalization
 do_normalization = True 
 
@@ -26,9 +31,10 @@ min_val = 0
 max_val = 99.9
 
 # Flag if your files follow the old file / folder naming convention
-flag_old_format = True
+flag_old_format = False
 
 ## MAIN CODE, DO NOT EDIT
+
 for folder in input_folders:
 
     if flag_old_format:
@@ -40,21 +46,34 @@ for folder in input_folders:
         underscores_to_z_plane = 2
         channel_wavelengths = {0:"488", 1:"561", 2:"640"}
         channel_folder = Path(folder / f"Ex_{channel_wavelengths.get(channel)}_Ch{channel}_stitched//")
-    
 
-    if do_normalization:
-        MIP_output_folder = channel_folder.parent / f"{channel_folder.name}_MIP{mip_thickness}um_min{min_val}_max{max_val}"
-        create_mips_from_folder(channel_folder, MIP_output_folder, z_step_size, mip_thickness, underscores_to_z_plane)
+    if create_MIPs:
+        if do_normalization:
+            MIP_output_folder = channel_folder.parent / f"{channel_folder.name}_MIP{mip_thickness}um_min{min_val}_max{max_val}"
+            create_mips_from_folder(channel_folder, MIP_output_folder, z_step_size, mip_thickness, underscores_to_z_plane)
 
-        MIP_images = sorted(MIP_output_folder.glob("*.tif"))
-        
-        for image in MIP_images:
-            normalized_image = normalize_image(image, min_val, max_val)
-            cv2.imwrite(str(image), normalized_image)
+            MIP_images = sorted(MIP_output_folder.glob("*.tif"))
+            
+            for image in MIP_images:
+                normalized_image = normalize_image(image, min_val, max_val)
+                cv2.imwrite(str(image), normalized_image)
+        else:
+            MIP_output_folder = channel_folder.parent / f"{channel_folder.name}_MIP{mip_thickness}um"
+            create_mips_from_folder(channel_folder, MIP_output_folder, z_step_size, mip_thickness, underscores_to_z_plane)
+
     else:
-        MIP_output_folder = channel_folder.parent / f"{channel_folder.name}_MIP{mip_thickness}um"
-        create_mips_from_folder(channel_folder, MIP_output_folder, z_step_size, mip_thickness, underscores_to_z_plane)
 
-    
+        if do_normalization:
+            img_output_folder = channel_folder.parent / f"{channel_folder.name}_normalized_min{min_val}_max{max_val}"
+            shutil.copytree(channel_folder, img_output_folder)
+
+            images = sorted(img_output_folder.glob("*.tif"))
+
+            for image in images:
+                normalized_image = normalize_image(image, min_val, max_val)
+                cv2.imwrite(str(image), normalized_image)
+        else:
+            print("MIP creation and normalization set to False. Nothing to do here...")
+        
 
 
