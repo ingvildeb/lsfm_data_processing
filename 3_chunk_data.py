4ff1e650a8c4dbdd6d4d075183493b769b573d58
Ingvild Bjerke <ingvieb@gmail.com>
2026-01-27 16:23:15 -0500

    Update 3_chunk_data.py
    Added documentation.

--- 3_chunk_data.py
+++ 3_chunk_data.py
@@ -2,22 +2,37 @@ from pathlib import Path
 from utils import chunk_image, chunk_z_stack
 import tifffile
 
+"""
+Written by: Ingvild Bjerke
+Last modified: 1/27/2026
+
+Purpose: Chunk images for use with cellpose. 
+The script takes a folder of images and creates chunks of a user-defined size.
+Works with single-plane images, MIPs or z stacks. Also works for atlas section images (which can be created in step 2a).
+
+Chunk images will be saved to individual folders for each parent image and named after the row and column indices from the
+parent image.
+
+"""
+
 # USER PARAMETERS
 
 # Give the path to the files you would like to chunk
 # The path can be to raw images or atlas slices
-file_path = Path(r"Z:\Labmembers\Ingvild\Testing_CellPose\test_3d\test_iba1\\")
+file_path = Path(r"Z:\Labmembers\Ingvild\Cellpose\Iba1_model\2_validation_images\\")
 
 # Define the chunk size. 
-# Smaller chunks (up to 128) are preferred for more dense signals (NeuN, Sytox)
-# Larger chunks (e.g. 256, 512) could be useful for medium to very sparse signals
+# NB: The network expects a chunk size of 256. It is not recommended to use chunks below this size for training,
+# as it can cause problems when applying the model to larger images.
+# Larger chunks (e.g. 512) could be useful for medium to very sparse signals, but if 256 works for your signal it 
+# is the safest option.
 chunk_size = 256
 
 
 # MAIN CODE, do not edit
 
 # Glob for TIFF files using pathlib
-files = file_path.glob("*.tif")
+files = file_path.glob("*.tif") 
 
 # Process each file
 for file in files:
@@ -40,3 +55,4 @@ for file in files:
 
     elif len(shape) == 3:
         chunk_z_stack(file, image_outdir, chunk_size=chunk_size)
+    
